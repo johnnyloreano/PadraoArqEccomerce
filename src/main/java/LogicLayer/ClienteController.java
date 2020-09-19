9c063e01b77646ed425dad97f2c04ed48e98945a
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-09-19 17:45:42 -0300

    Cliente Controller, Model, Persistence

---
+++ src/main/java/LogicLayer/ClienteController.java
@@ -0,0 +1,28 @@
+package LogicLayer;
+
+import DataLayer.ClienteModel;
+import PersistenceLayer.ClientePersistence;
+
+public class ClienteController {
+
+    private ClientePersistence clientePersistence;
+    ClienteController(ClientePersistence clientePersistence){
+        this.clientePersistence = clientePersistence;
+    }
+
+    public ClienteModel getClienteByCPF(ClienteModel clienteModel){
+
+        ClienteModel resultCliente = clientePersistence.getClienteByCPF(clienteModel.getCpf());
+        if(resultCliente == null)
+            throw new NullPointerException("Cliente não encontrado!");
+        return resultCliente;
+
+    }
+
+    public void cadastraCliente(ClienteModel newCliente){
+
+        this.clientePersistence.addCliente(newCliente);
+        
+    }
+
+}
---
+++ src/main/java/PersistenceLayer/ClientePersistence.java
@@ -0,0 +1,34 @@
+package PersistenceLayer;
+
+import DataLayer.ClienteModel;
+
+import java.util.List;
+
+public class ClientePersistence {
+
+    private List<ClienteModel> clientList;
+
+    ClientePersistence(List<ClienteModel> list){
+        this.clientList = list;
+    }
+    public void init(){
+        this.clientList.add(new ClienteModel("John", "123", "Brazil"));
+        this.clientList.add(new ClienteModel("Mary", "855", "Swiss"));
+        this.clientList.add(new ClienteModel("Doe", "902", "Chile"));
+        this.clientList.add(new ClienteModel("Luka", "012", "Czech"));
+        this.clientList.add(new ClienteModel("Kevin", "321", "Brazil"));
+    }
+    public ClienteModel getClienteByCPF(String cpf){
+        if(cpf == null) throw new NullPointerException("CPF usado como argumento é nulo");
+        for (ClienteModel client : this.clientList){
+            if(client.getCpf().equals(cpf) return client;
+        }
+        return null;
+    }
+
+    public void addCliente(ClienteModel newClient){
+        if(newClient == null) throw new NullPointerException("Cliente usado como argumento é nulo");
+        this.clientList.add(newClient);
+    }
+
+}
